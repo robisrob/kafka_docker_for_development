ccc0b525b98fcf224aff3f0b27fdd62989ac40cf
Bart De Neuter <bart.d@sparkcentral.com>
2018-03-18 12:48:06 +0100

    Make the Kafka broker reachable from outside and inside the docker network

--- context/configureKafka.sh
+++ context/configureKafka.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i "s~#listeners=.*~listeners=$KAFKA_LISTENERS~" /app/kafka_2.11-1.0.1/config/server.properties
-sed -i "s~#advertised.listeners=.*~advertised.listeners=$KAFKA_ADVERTISED_LISTENERS~" /app/kafka_2.11-1.0.1/config/server.properties
-sed -i "s~#listener.security.protocol.map=.*~listener.security.protocol.map=PLAINTEXT:PLAINTEXT,INTERNAL:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL~" /app/kafka_2.11-1.0.1/config/server.properties
+sed -i "s~#listeners=.*~listeners=$KAFKA_LISTENERS~" /app/kafka_2.11-0.10.2.0/config/server.properties
+sed -i "s~#advertised.listeners=.*~advertised.listeners=$KAFKA_ADVERTISED_LISTENERS~" /app/kafka_2.11-0.10.2.0/config/server.properties
+sed -i "s~#listener.security.protocol.map=.*~listener.security.protocol.map=PLAINTEXT:PLAINTEXT,INTERNAL:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL~" /app/kafka_2.11-0.10.2.0/config/server.properties
 
 
